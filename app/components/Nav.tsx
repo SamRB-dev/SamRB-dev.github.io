93a25f88ccb60f04ceec7da2a2a61c0e6b8d0caf
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-02-23 22:14:32 +0100

    init

---
+++ app/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="flex">
+      <a href="#">Contact</a>
+      <a href="#">Resume</a>
+      <a href="#">About</a>
+      <a href="#">Certificate</a>
+      <a href="#">Projects</a>
+    </nav>
+  );
+}
+
+export default Nav;
