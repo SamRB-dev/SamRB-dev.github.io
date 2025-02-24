e2e40eed9fcf239e43435404e9e73ca4e3d7275b
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-02-24 21:07:38 +0100

    wip

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,17 +7,18 @@ function HeroSection() {
   return (
     <>
       <div className="flex justify-center mb-[4%]">
-        {/* <Image
-          src="https://placehold.co/600x400"
+        <Image
+          src="https://fakeimg.pl/500x500"
           alt="Main Image"
           width={500}
           height={500}
-        /> */}
-        <img
+          className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,1),-15px_-8px_45px_0px_rgba(0,0,0,0.1)]"
+        />
+        {/* <img
           src="https://fakeimg.pl/500x500"
           className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,1),-15px_-8px_45px_0px_rgba(0,0,0,0.1)]"
           alt=""
-        />
+        /> */}
       </div>
       <div className="flex-col justify-center text-white">
         <h1 className="text-center mb-[2%] text-4xl">Hey</h1>
