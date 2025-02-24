9da68cd8457a98658550065fe694c20981fc3eff
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-02-24 21:01:11 +0100

    WIP

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",  // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
