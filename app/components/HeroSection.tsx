ff9aa5f5083afe98a4eaa3791ef8c1e5ed52ae84
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-02-25 13:03:17 +0100

    HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,21 +2,25 @@
 import Image from "next/image";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import Main from "../../public/Main.jpg";
+import Main_2 from "../../public/Main-2.jpg";
 
 function HeroSection() {
+  let images: any = [Main, Main_2];
   return (
     <>
       <div className="flex justify-center mb-[4%]">
         <Image
-          src="https://fakeimg.pl/500x500"
+          // src="https://fakeimg.pl/500x500"
+          src={images[Math.floor(Math.random() * 2)]}
           alt="Main Image"
           width={500}
           height={500}
-          className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,1),-15px_-8px_45px_0px_rgba(0,0,0,0.1)]"
+          className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,0.3),-15px_-8px_45px_0px_rgba(0,0,0,0.3)]"
         />
         {/* <img
           src="https://fakeimg.pl/500x500"
-          className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,1),-15px_-8px_45px_0px_rgba(0,0,0,0.1)]"
+          className="rounded-full shadow-[15px_8px_45px_0px_rgba(0,0,0,1),-15px_-8px_45px_0px_rgba(0,0,0,0.3)]"
           alt=""
         /> */}
       </div>
@@ -25,8 +29,8 @@ function HeroSection() {
         <h1 className="text-center font-bold text-6xl mb-[1.2%]">
           I'm Sadim Rahman
         </h1>
-        <h1 className="text-center font-bold text-5xl">
-          I'm a    
+        <h1 className="text-center font-bold text-5xl mb-[2.5%]">
+          I'm a
           <span>
             <TypeAnimation
               className="text-center font-bold text-5xl text-[#D32D20]"
@@ -45,6 +49,22 @@ function HeroSection() {
             />
           </span>
         </h1>
+        <h3 className="text-center w-[50%] m-auto text-xl mb-[3%]">
+          A passionate electronic engineering student with a strong focus on
+          software development, machine learning, and AI. Continuously exploring
+          real-world applications through hands-on projects, from deep learning
+          models to AI-powered solutions. Actively seeking opportunities to
+          apply my skills and contribute to innovative advancements in the
+          field.
+        </h3>
+        <div id="url" className="flex justify-center">
+          <a
+            href="#"
+            className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.5),-4px_-4px_2px_0px_rgba(255,255,255,0.5)]"
+          >
+            Contact
+          </a>
+        </div>
       </div>
     </>
   );
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,31 +5,31 @@ function Nav() {
     <nav className="flex justify-around gap-2 mb-[11%]">
       <a
         href="#"
-        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.1),-4px_-4px_2px_0px_rgba(255,255,255,.1)]"
+        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.3),-4px_-4px_2px_0px_rgba(255,255,255,0.3)]"
       >
         Contact
       </a>
       <a
         href="#"
-        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.1),-4px_-4px_2px_0px_rgba(255,255,255,.1)]"
+        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.3),-4px_-4px_2px_0px_rgba(255,255,255,0.3)]"
       >
         Resume
       </a>
       <a
         href="#"
-        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.1),-4px_-4px_2px_0px_rgba(255,255,255,.1)]"
+        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.3),-4px_-4px_2px_0px_rgba(255,255,255,0.3)]"
       >
         About
       </a>
       <a
         href="#"
-        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.1),-4px_-4px_2px_0px_rgba(255,255,255,.1)]"
+        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.3),-4px_-4px_2px_0px_rgba(255,255,255,0.3)]"
       >
         Certificate
       </a>
       <a
         href="#"
-        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.1),-4px_-4px_2px_0px_rgba(255,255,255,.1)]"
+        className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.3),-4px_-4px_2px_0px_rgba(255,255,255,0.3)]"
       >
         Projects
       </a>
