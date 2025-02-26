de6bc00c49c8710fcdd0d5a5dfd1cbe75f1635de
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-02-26 10:20:30 +0100

    WIP

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -62,7 +62,7 @@ function HeroSection() {
             href="#"
             className="px-8 py-3 rounded-full text-[22px] font-medium bg-[#24272C] shadow-[4px_4px_2px_0px_rgba(0,0,0,0.5),-4px_-4px_2px_0px_rgba(255,255,255,0.5)]"
           >
-            Contact
+            Contact me
           </a>
         </div>
       </div>
---
+++ app/components/SkillSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CentralImage from "../../public/Rectangle 75.svg";
+function SkillSection() {
+  let skill_list_orbit_1 = [
+    {
+      python:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg",
+    },
+  ];
+  return <div>SkillSection</div>;
+}
+
+export default SkillSection;
